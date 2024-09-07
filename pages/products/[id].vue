a0bb0e052bec4eb730cbd5b10d41b92ffb9542af
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-07 07:01:52 +0530

    feat: create a dynamic page for every product with id

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Product details {{ id }}</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis doloribus eveniet unde ullam iusto nihil
+            distinctio suscipit commodi similique vitae!</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+definePageMeta({
+    layout: "products"
+})
+</script>
+
+<style scoped></style>
