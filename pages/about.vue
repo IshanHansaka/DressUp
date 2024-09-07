0cde508a98516e100aeb38c53ac96343de93ca51
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-07 07:00:47 +0530

    feat: create about page

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea cum ex harum dolore esse dolores laboriosam saepe
+            ipsum eaque, ducimus eius! Eum magni praesentium ipsum ratione ex ipsam distinctio adipisci!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+}
+</style>
