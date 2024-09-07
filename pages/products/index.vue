d05226d5d1e2e182c5f54542f9de9adba197dcad
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-07 07:41:41 +0530

    feat: featch fackstore api with useFetch and get product data and display all products and single product

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <p>Product</p>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="product in products">
+                <NuxtLink :to="`/products/${product.id}`">{{ product.title }}</NuxtLink>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,6 +12,9 @@
 definePageMeta({
     layout: "products"
 })
+
+//fetch products
+const { data: products } = await useFetch("https://fakestoreapi.com/products")
 </script>
 
 <style scoped></style>
