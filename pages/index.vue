8c0a4b2f5c6e0cd1dd3a752540f2ea63464f13c8
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-09-15 21:12:57 +0530

    chore: change index.vue content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <h1>Helo world</h1>
+        <h1>DressUp</h1>
+        <p>A simple online store built with Nuxt 3 and Fake Store API as a learning platform. The project displays
+            products and allows users to click on each item to view detailed pages with the product's title, price, and
+            description.</p>
     </div>
 </template>
 
